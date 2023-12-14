5b4804521c289b3db04dc05c442510250758dae2
Max D <maksimdoronin93@gmail.com>
2023-12-14 07:04:08 +0200

    chore: add link to home

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Text } from '../components'
 
 export const SettingsRoute = () => {
@@ -6,6 +7,9 @@ export const SettingsRoute = () => {
 			<Text as={'h1'} variant={'h1'}>
 				Settings page
 			</Text>
+			<Link to={'/settings'}>
+				back to home
+			</Link>
 		</>
 	)
 }
